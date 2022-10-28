9c89a35d9fc93dc06904413188351d158923dcde
mfaishal882 <mfaishal882@gmail.com>
2022-10-28 13:01:16 +0700

    delete mock

--- feature/user/services/services_test.go
+++ feature/user/services/services_test.go
@@ -1,51 +1,12 @@
 package services
 
-<<<<<<< HEAD
-import (
-	"be_project3team3/config"
-	"be_project3team3/feature/user/domain"
-	"be_project3team3/feature/user/mocks"
-	"errors"
-	"testing"
-=======
-// func TestRegister(t *testing.T) {
-// 	repo := mocks.NewRepository(t)
-// 	t.Run("Sukses Insert", func(t *testing.T) {
-// 		repo.On("Insert", mock.Anything).Return(domain.Core{ID: uint(1), Username: "same", Email: "skjsa"}, nil).Once()
-// 		srv := New(repo)
-// 		input := domain.Core{Username: "same", Email: "skjsa", Password: "same"}
-// 		c := echo.Context
-// 		res, err := srv.Register(input, c)
-// 		assert.Nil(t, err)
-// 		assert.NotEmpty(t, res.ID, "Seharusnya ada ID yang berhasil dibuat")
-// 		assert.NotEqual(t, res.Password, input.Password, "Password tidak terenkripsi")
-// 		assert.Equal(t, input.Username, res.Username, "Nama user harus sesuai")
-// 		repo.AssertExpectations(t)
-// 	})
-// 	t.Run("Duplicated", func(t *testing.T) {
-// 		repo.On("Insert", mock.Anything).Return(domain.Core{}, errors.New(config.DUPLICATED_DATA)).Once()
-// 		srv := New(repo)
-// 		input := domain.Core{Username: "same", Email: "skjsa", Password: "same"}
-// 		c := echo.Context
-// 		res, err := srv.Register(input, echo.Context)
-// 		assert.NotNil(t, err)
-// 		assert.EqualError(t, err, config.DUPLICATED_DATA, "Pesan error tidak sesuai")
-// 		assert.Equal(t, uint(0), res.ID, "ID seharusnya 0 karena gagal input data")
-// 		repo.AssertExpectations(t)
-// 	})
-// 	t.Run("Gaga; Hash", func(t *testing.T) {
-// 		repo.On("Insert", mock.Anything).Return(domain.Core{}, errors.New("cannot encript password")).Once()
-// 		srv := New(repo)
-// 		input := domain.Core{Username: "same", Email: "skjsa", Password: ""}
-// 		res, err := srv.Register(input, echo.Context)
-// 		assert.NotNil(t, err)
-// 		assert.EqualError(t, err, "cannot encript password", "Pesan error tidak sesuai")
-// 		assert.Equal(t, uint(0), res.ID, "ID seharusnya 0 karena gagal input data")
-// 		repo.AssertExpectations(t)
-// 	})
-// }
->>>>>>> order, fix, s3
-
+// import (
+// 	"be_project3team3/config"
+// 	"be_project3team3/feature/user/domain"
+// 	"be_project3team3/feature/user/mocks"
+// 	"errors"
+// 	"testing"
+// )
 // func TestUpdate(t *testing.T) {
 // 	repo := mocks.NewRepository(t)
 // 	t.Run("Sukses Update", func(t *testing.T) {
